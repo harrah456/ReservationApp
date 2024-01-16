36251f9637a7cdce06cc4f3fca09c2bdee2e2bef
harrah456 <darkdevils789@gmail.com>
2024-01-16 08:12:00 +0600

    Added holiday viewmodels and search page

---
+++ ReserveApp/Data/HolidayViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ReserveApp.Data
+{
+    public class HolidaySaveModel
+    {
+        public Hotel Hotel { get; set; }
+        public Tour Tour { get; set; }
+    }
+
+    public class HolidaySearchModel
+    {
+        public IEnumerable<Hotel> Hotels { get; set; }
+
+        public IEnumerable<Tour> Tours { get; set; }
+    }
+}
